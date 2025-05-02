5e8624908b4dc14512b161e38dd275985c87b7db
Mike Green <mike.is.green@gmail.com>
2025-05-02 16:42:17 -0400

    Finish server REST API

--- index.js
+++ index.js
@@ -6,29 +6,41 @@ import { setupIntercom } from './lib/setup-intercom.js';
 
 const NODE_ENV = process.env?.NODE_ENV || 'development';
 
+const { combine, timestamp, cli, json, simple } = winston.format;
+
 const app = new Koa();
 const router = new Router();
 const logger = winston.createLogger({
   level: process.env?.LOG_LEVEL || 'info',
-  format: NODE_ENV === 'development' ? winston.format.simple() : winston.format.json(),
+  format: NODE_ENV === 'development' ? combine(timestamp(), cli(), simple()) : combine(timestamp(), json()),
   transports: [new winston.transports.Console()],
 });
 
-const connectionManager = WebRtcConnectionManager.create({
-  beforeOffer(peerConnection) {
-    setupIntercom(peerConnection);
-  },
-});
+const connectionManager = new WebRtcConnectionManager();
 
 function cleanup() {
   for (const connection of connectionManager.connections.values()) {
     connection.close();
   }
-  process.exit();
+  process.exit(0);
 }
 
 process.on('SIGINT', cleanup);
 process.on('SIGTERM', cleanup);
+
+const getConnection = async (ctx, next) => {
+  const { id } = ctx.params;
+  ctx.state = ctx.state || {};
+  const connection = connectionManager.getConnection(id);
+
+  if (connection) {
+    ctx.state.connection = connection;
+  } else {
+    ctx.status = 404;
+  }
+
+  next();
+};
 
 router.get(
   '/connections',
@@ -41,6 +53,87 @@ router.get(
   (ctx) => {
     logger.info('GET /connections', { connections: ctx.state.connections.keys() });
   },
+);
+
+router.post(
+  '/connections',
+  async (ctx, next) => {
+    try {
+      const connection = await connectionManager.createConnection({
+        beforeOffer: setupIntercom,
+      });
+      ctx.state.connections = connectionManager.getConnections();
+      ctx.body = JSON.stringify(connection.toJSON());
+    } catch (error) {
+      logger.error(error);
+      ctx.status = 500;
+    } finally {
+      next();
+    }
+  },
+  (ctx) => logger.info('POST /connections', { connections: ctx.state.connections?.keys() }),
+);
+
+router.get(
+  '/connections/:id',
+  getConnection,
+  async (ctx, next) => {
+    next();
+
+    const { connection } = ctx.state;
+
+    if (connection) {
+      ctx.body = JSON.stringify(ctx.state.connection.toJSON());
+    }
+  },
+  (ctx) => logger.info(`GET /connections/${ctx.params.id}`, { status: ctx.status }),
+);
+
+router.delete(
+  '/connections/:id',
+  getConnection,
+  async (ctx, next) => {
+    next();
+
+    const { connection } = ctx.state;
+
+    if (connection) {
+      connection.close();
+      logger.info(`Close connection ${ctx.params.id}`);
+      ctx.body = JSON.stringify(connection.toJSON());
+    }
+  },
+  (ctx) => logger.info(`DELETE /connections/${ctx.params.id}`, { status: ctx.status }),
+);
+
+router.get(
+  '/connections/:id/local-description',
+  getConnection,
+  async (ctx, next) => {
+    next();
+
+    const { connection } = ctx.state;
+
+    if (connection) {
+      ctx.body = JSON.stringify(connection.toJSON().localDescription);
+    }
+  },
+  (ctx) => logger.info(`GET /connections/${ctx.params.id}/local-description`, { status: ctx.status }),
+);
+
+router.get(
+  '/connections/:id/remote-description',
+  getConnection,
+  async (ctx, next) => {
+    next();
+
+    const { connection } = ctx.state;
+
+    if (connection) {
+      ctx.body = JSON.stringify(connection.toJSON().remoteDescription);
+    }
+  },
+  (ctx) => logger.info(`GET /connections/${ctx.params.id}/remote-description`, { status: ctx.status }),
 );
 
 app.use(router.routes()).use(router.allowedMethods());
